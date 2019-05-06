9b0e65dfd042438ad89394035c78af0d031952b1
Mani M <mani@cogknit.com>
2019-05-06 18:31:24 +0530

    modified findbyusername to suport userid or email

--- SpringSecurityForRest/src/main/java/com/example/session/dbauth/repository/UsersRepository.java
+++ SpringSecurityForRest/src/main/java/com/example/session/dbauth/repository/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.example.session.dbauth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.session.dbauth.model.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+	@Query(value = "select u from Users u where (u.email=?1 OR u.id=?1)", nativeQuery = false)
 	Users findByName(String username);
 }
