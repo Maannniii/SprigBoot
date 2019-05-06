755da49af3b5495f9a496ecdd44909089098f91f
Mani M <mani@cogknit.com>
2019-05-06 17:54:03 +0530

    added login api

--- SpringSecurityForRest/src/main/java/com/example/session/dbauth/controller/UnauthController.java
+++ SpringSecurityForRest/src/main/java/com/example/session/dbauth/controller/UnauthController.java
@@ -1,6 +1,17 @@
 package com.example.session.dbauth.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ua")
 public class UnauthController {
 
+	@Autowired
+	AuthenticationManager authManager;
+
+	@Autowired
+	HttpSession session;
+
 	@GetMapping("/hello")
 	public String unauthhello() {
 		return "Hello world from UnAuth";
+	}
+
+	@PostMapping("/login")
+	public Map<String, Object> login(@RequestBody Map<String, Object> map) {
+		Authentication auth = authManager.authenticate(new UsernamePasswordAuthenticationToken(
+				map.get("username").toString(), map.get("password").toString()));
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		map.put("sessionid", session.getId());
+		return map;
 	}
 }
