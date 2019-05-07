58df4234b35e7400933e0ce4fd5cb18c37161758
Mani M <mani@cogknit.com>
2019-05-07 11:45:46 +0530

    disabled formlogin and http basic auth and changes session creation policy to manual

--- SpringSecurityForRest/src/main/java/com/example/session/dbauth/config/SessionSecurityConfig.java
+++ SpringSecurityForRest/src/main/java/com/example/session/dbauth/config/SessionSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,7 @@ import com.example.session.dbauth.service.CustomUserDetailsService;
 @EnableJpaRepositories(basePackageClasses = UsersRepository.class)
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SessionSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -39,13 +41,13 @@ public class SessionSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// https://www.baeldung.com/spring-security-session
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
 		http.csrf().disable().authorizeRequests() //
 				.antMatchers("/ua/*").permitAll() //
 				.antMatchers("/auth/*").authenticated() //
 				.and().requestCache() //
 				.requestCache(new NullRequestCache()) //
-				.and().httpBasic().disable().formLogin().disable();
+				.and().httpBasic().disable().formLogin().disable().logout().disable();
 	}
 
 	private PasswordEncoder getPasswordEncoder() {
